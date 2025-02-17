33ce44a5fa9e578e1c94adfa8d53f8f0a83a56f5
Peter Kagwe <kagwepeter07@gmail.com>
2025-02-17 18:06:17 +0300

    update indexer

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -8,5 +8,5 @@ export const TORII_URL = "https://command-nexus.xyz"
 
 
 
-export const NEXUS_ADDRESS = "0x6dfc0ff2cb0e5c8cb210e992f2ae75983b00c0ec4bf3398cf8188164e3b1346"
-export const ARENA_ADDRESS = "0x33432826d34d86f17f382eaeeef3005b1970d47afdfef21f6a438123627017c"
+export const NEXUS_ADDRESS = "0x3e53962a57327c91b5ef7579d937ebdea359e7ba6bec0a5ee5d6206bf099a59"
+export const ARENA_ADDRESS = "0x6965f4463311163ecb9908df6cc7592f87166fba982f221da83172ecc05778d"

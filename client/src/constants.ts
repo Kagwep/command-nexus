f3fe651e4a69e1184493254bdbcac3523d724ab6
Peter Kagwe <kagwepeter07@gmail.com>
2025-02-25 18:39:07 +0300

    update indexer

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -8,5 +8,5 @@ export const TORII_URL = "https://api.cartridge.gg/x/command-nexus-9/torii"
 
 
 
-export const NEXUS_ADDRESS = "0x195a792a25b5d96f553fae502aad6b8e399b6af8a38d5fa10d164bdb089d582"
-export const ARENA_ADDRESS = "0x1e6b8c57f7df233de5c7bbe6d1951e7c41a233840b1eaa73bc581b5de945268"
+export const NEXUS_ADDRESS = "0x6b8a63d94c433f18073d10fe66c2f9aca17965f5f870c4591a9920d1828623f"
+export const ARENA_ADDRESS = "0x664ccd0f877d1f8273c70ce4bb53f3eae89f5a86650cb247d1df3f04a1bdb56"

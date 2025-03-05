2fa5153afacede9cb4c8df67a8237a6fc89f7842
Peter Kagwe <kagwepeter07@gmail.com>
2025-03-05 20:12:32 +0300

    update torii indexer

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -3,10 +3,10 @@ import manifest from '../manifest_sepolia.json'
 
 export const TORII_RPC_URL = "https://api.cartridge.gg/x/starknet/sepolia"
 export const WORLD_ADDRESS = manifest.world.address
-export const TORII_URL = "https://api.cartridge.gg/x/command-nexus-9/torii"
+export const TORII_URL = "https://api.cartridge.gg/x/command-nexus-10/torii"
 
 
 
 
-export const NEXUS_ADDRESS = "0x47bf71d0ba231be007cd1bdb887e0e25354090a90dae79e48dc9739e2edcf08"
-export const ARENA_ADDRESS = "0x70c6c627b8c540d14b159c2741e336b0726c70510eb3be62685be4003e035c"
+export const NEXUS_ADDRESS = "0x5c50a92b3a9608da4fddc267438f705860a8921b5d7d228d1a2bb722e854b6c"
+export const ARENA_ADDRESS = "0x1e822ce8ed4b685dc790e5a93b42f7a3158a741cb9558a64347bd68432a7174"
